d466e0d7a83ba689ba1290fbb2f4a0fb7260e96b
flat-resonanse <flat-resonance@stellarterm.com>
2021-04-16 12:15:29 +0300

    Change directory logic

--- directory.js
+++ directory.js
@@ -1,68 +1,205 @@
-// This code is licensed under Apache-2.0.
-// It is released AS-IS and without warranty.
-// This is simply for informational purposes. The creators of this file does
-// not endorse any of this information.
+import req from './req';
 
-const anchors = require('./static/anchors.json');
-const destinations = require('./static/destinations.json');
+const MEMO_TYPES = new Set(['MEMO_TEXT', 'MEMO_ID', 'MEMO_HASH', 'MEMO_RETURN']);
 
-const DirectoryBuilder = require('./source/DirectoryBuilder');
+class DirectoryClass {
+    constructor() {
+        this.anchors = {};
+        this.destinations = {};
+        this.assets = {};
+        this.issuers = {};
+        this.pairs = {};
+        this.initializationRequest = null;
+        this.buildID = null;
 
-const directory = new DirectoryBuilder();
-directory.DirectoryBuilder = DirectoryBuilder;
+        // Special anchors aren't really anchors at all!
+        this.nativeAnchor = {
+            name: 'Stellar Network',
+            website: 'https://www.stellar.org/lumens/',
+            logo: '/anchors/logos/stellar.png',
+            color: '#000000',
+        };
+        this.nativeAsset = {
+            code: 'XLM',
+            issuer: null,
+            domain: 'native',
+        };
 
-// For adding anchors and assets to directory use anchors.json,
-// add anchor data and array of assets for this anchor.
-// For "fiat" assets add field "isCounterSelling": "true"
-// Template:
-// {
-//     "domain": "DOMAINDOTCOM',
-//     "website": "https://DOMAINDOTCOM',
-//     "logo": "DOMAINDOTCOM',
-//     "color": "#rrggbb',
-//     "displayName": "Domain dot com',
-//     "assets": [
-//     {
-//         "code": "ASSETCODE_ASSETCODE',
-//         "issuer": "Ga_issuer_account_id',
-//         "type": "token",
-//         "isCounterSelling": "true",
-//     }, ...,  {}]
-// },
-
-anchors.forEach((anchor) => {
-    directory.addAnchor(anchor);
-    anchor.assets.forEach((asset) => {
-        directory.addAsset(anchor.domain, asset);
-        const isCounterAsset = asset.code === 'BTC' || asset.isCounterSelling;
-        directory.addPair({
-            baseBuying: isCounterAsset ? ['XLM', 'native'] : [asset.code, anchor.domain],
-            counterSelling: isCounterAsset ? [asset.code, anchor.domain] : ['XLM', 'native'],
-        });
-    });
-});
-
-destinations.forEach((destination) => {
-    const { id, data } = destination;
-    directory.addDestination(id, data);
-});
-
-// Assert that each asset has a trading pair
-const remainingAssets = Object.assign({}, directory.assets);
-
-const pairIds = Object.keys(directory.pairs);
-pairIds.forEach((pairId) => {
-    const pair = directory.pairs[pairId];
-    if (pair.baseBuying.code === 'XLM' && pair.baseBuying.issuer === null) {
-        delete remainingAssets[`${pair.counterSelling.code}-${pair.counterSelling.issuer}`];
-    } else if (pair.counterSelling.code === 'XLM' && pair.counterSelling.issuer === null) {
-        delete remainingAssets[`${pair.baseBuying.code}-${pair.baseBuying.issuer}`];
+        this.unknownAnchor = {
+            name: 'unknown',
+            logo: '/anchors/logos/unknown.png',
+            assets: {},
+        };
     }
-});
 
-const remainingAssetKeys = Object.keys(remainingAssets);
-if (remainingAssetKeys.length) {
-    throw new Error(`Missing trading pair. Please use addPair() for asset: ${remainingAssetKeys[0]}`);
+    initialize(url) {
+        if (!this.initializationRequest) {
+            this.initializationRequest = req.getJson(url).then(data => {
+                data.anchors.forEach(anchor => this.addAnchor(anchor));
+                data.destinations.forEach(destination => this.addDestination(destination));
+                this.buildID = data.buildId;
+            });
+        }
+        return this.initializationRequest;
+    }
+
+    addAnchor(anchor) {
+        if (!this.validateAnchor(anchor)) {
+            console.warn(`Anchor ${anchor.domain} is invalid, it will be skipped`);
+            return;
+        }
+        // add anchor
+        this.anchors[anchor.domain] = {
+            name: anchor.domain,
+            displayName: anchor.displayName,
+            support: anchor.support,
+            website: anchor.website,
+            logo: anchor.logo,
+            assets: {},
+        };
+        if (anchor.color) {
+            this.anchors[anchor.domain].color = anchor.color;
+        }
+
+        // add assets
+        anchor.assets.forEach(asset => {
+            const slug = `${asset.code}-${asset.issuer}`;
+            this.assets[slug] = Object.assign({}, asset, { domain: anchor.domain });
+            this.anchors[anchor.domain].assets[asset.code] = slug;
+            if (!this.issuers[asset.issuer]) {
+                this.issuers[asset.issuer] = {};
+            }
+            this.issuers[asset.issuer][asset.code] = slug;
+
+            // add pair for asset
+            const isCounterAsset = asset.code === 'BTC' || asset.isCounterSelling;
+            const assetData = {
+                domain: anchor.domain,
+                code: asset.code,
+                issuer: asset.issuer,
+            };
+            const baseAsset = isCounterAsset ? this.nativeAsset : assetData;
+            const counterAsset = isCounterAsset ? assetData : this.nativeAsset;
+            const pairId = `${baseAsset.code}-${baseAsset.domain}/${counterAsset.code}-${counterAsset.domain}`;
+
+            this.pairs[pairId] = {
+                baseBuying: {
+                    code: baseAsset.code,
+                    issuer: baseAsset.issuer,
+                },
+                counterSelling: {
+                    code: counterAsset.code,
+                    issuer: counterAsset.issuer,
+                },
+            };
+        });
+    }
+
+    validateAnchor(anchor) {
+        let isValid = true;
+        if (!anchor.domain) {
+            console.error('Missing anchor domain');
+            isValid = false;
+        }
+        if (this.anchors[anchor.domain] !== undefined) {
+            console.error(`Duplicate anchor in directory: ${anchor.domain}`);
+            isValid = false;
+        }
+        if (!anchor.logo === undefined) {
+            console.error(`Missing logo file: ${anchor.logo}`);
+            isValid = false;
+        }
+        if (anchor.website.indexOf('http://') !== -1) {
+            console.error('Website URL must use https://');
+            isValid = false;
+        }
+        if (anchor.website.indexOf(anchor.domain) === -1) {
+            console.error('Website URL of anchor must contain the anchor domain');
+            isValid = false;
+        }
+        if (!anchor.displayName) {
+            console.error(`Display name is required for anchor: ${anchor.domain}`);
+            isValid = false;
+        }
+        if (anchor.color && !anchor.color.match(/^#([A-Fa-f0-9]{6})/)) {
+            console.error(`Color must be in hex format with 6 characters (example: #c0ffee). Got: ${anchor.color}`);
+            isValid = false;
+        }
+        const assets = anchor.assets.map(({ code }) => code);
+        if (assets.length !== new Set(assets).size) {
+            console.error('All anchors assets codes must be unique');
+            isValid = false;
+        }
+        anchor.assets.forEach((asset) => {
+            if (!Object.prototype.hasOwnProperty.call(asset, 'code') || !Object.prototype.hasOwnProperty.call(asset, 'issuer')) {
+                console.error('Missing assets issuer or code');
+                isValid = false;
+            }
+            const slug = `${asset.code}-${asset.issuer}`;
+            if (Object.prototype.hasOwnProperty.call(this.assets, slug)) {
+                console.error(`Duplicate asset: ${slug}`);
+                isValid = false;
+            }
+            if (Object.prototype.hasOwnProperty.call(asset, 'unlisted') && asset.unlisted !== true) {
+                console.error(`Asset property unlisted must be unset or true: ${slug}`);
+                isValid = false;
+            }
+        });
+        return isValid;
+    }
+
+    addDestination(destination) {
+        const { id, data } = destination;
+        if (!data.name) {
+            throw new Error('Name required for destinations');
+        }
+        if (data.requiredMemoType && !MEMO_TYPES.has(data.requiredMemoType)) {
+            throw new Error('Invalid memo type when adding destination');
+        }
+        this.destinations[id] = Object.assign({}, data);
+    }
+
+    getAnchor(domain) {
+        if (domain === 'native') {
+            return this.nativeAnchor;
+        }
+        return this.anchors[domain] || this.unknownAnchor;
+    }
+
+    getAssetByDomain(code, domain) {
+        if (code === 'XLM' && domain === 'native') {
+            return this.nativeAsset;
+        }
+        const slug = this.anchors[domain] && this.anchors[domain].assets[code];
+        return slug ? {
+            code,
+            issuer: this.assets[slug].issuer,
+            domain,
+        } : null;
+    }
+
+    getAssetByAccountId(code, issuer) {
+        if (code === 'XLM' && issuer === null) {
+            return this.nativeAsset;
+        }
+        return this.assets[`${code}-${issuer}`] || null;
+    }
+
+    resolveAssetByAccountId(code, issuer) {
+        return this.getAssetByAccountId(code, issuer) || {
+            code,
+            issuer,
+            domain: 'unknown',
+        };
+    }
+
+    getAssetBySdkAsset(asset) {
+        return asset.isNative() ? this.nativeAsset : this.getAssetByAccountId(asset.getCode(), asset.getIssuer());
+    }
+
+    getDestination(accountId) {
+        return this.destinations[accountId];
+    }
 }
 
-module.exports = directory;
+export default new DirectoryClass();
